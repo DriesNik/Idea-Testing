7d8d5b0a2466487c11140a9471b273326a0af1b0
Nik Dries <ninja1704@hotmail.com>
2017-01-10 12:15:25 -0600

    first upload with dice

---
+++ TestingStuff/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<int> wow = new CustomList<int>();
+            wow.AddToList(5);
+            wow.AddToList(15);
+            wow.AddToList(25);
+            wow.PrintList();
+            Console.ReadLine();
+        }
+    }
+}
